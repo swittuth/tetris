5e8099712004f197feae2b21153568e5294d9496
swittuth <swittuthcoding@gmail.com>
2022-02-16 20:46:47 -0500

    added instruction button to display instructions to player

--- playGame.js
+++ playGame.js
@@ -1,8 +1,19 @@
 import * as game_board from "./board.js";
 
 const start_restart_button = document.getElementById("start-restart");
+const instruction_button = document.getElementById("instruction-button");
 
 start_restart_button.addEventListener("click", () => {
     game_board.initiate_game();
     start_restart_button.innerHTML = "RESTART";
 });
+
+instruction_button.addEventListener("click", () => {
+    window.alert(`    "a" or ← : move left
+    "d" or → : move right
+    "s" or ↓ : increase speed
+    "w" or ↑ : rotate block
+    "spacebar" : drop block
+    "shift": hold / swap block
+    "p" : pause game`)
+})
